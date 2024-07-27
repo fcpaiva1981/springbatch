9e1d2d51eabced3b721e570a045a9fdf8e74963c
Fernando Campos de Paiva <fernandocampospaiva@hotmail.com>
2024-07-27 14:18:51 -0300

    Add LeituraArquivoLarguraFixa
    Not save in DB

--- src/main/java/br/com/fcpaiva/com/br/springbatch/config/PropsConfig.java
+++ src/main/java/br/com/fcpaiva/com/br/springbatch/config/PropsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.FileSystemResource;
 
-import java.nio.file.FileSystem;
+
 
 @Configuration
 public class PropsConfig {
